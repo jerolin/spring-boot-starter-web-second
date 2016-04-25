07a8a768ce108bce026c00fb5b01d7745df72fc8
yujl <yujl@primeton.com>
2016-04-25 09:37:49 +0800

    update:修改启动脚本。

--- euler-demo-third/script/sh/start.sh
+++ euler-demo-third/script/sh/start.sh
@@ -3,27 +3,16 @@ clear
 echo Starting
 echo PATH="$PATH"
 WORKING_HOME="$(cd "$(dirname "$0")"/../; pwd)"
-PID_FILE="$WORKING_HOME/component.pid"
+PID_FILE="$WORKING_HOME/firstapp.pid"
 echo WORKING_HOME=$WORKING_HOME
-echo "***********"
-JAVA=`$WORKING_HOME/sh/find-java.sh 2>/dev/null`
-if [ "$JAVA"x == x ]
-	then
-		echo cannot find runnable binary java file, exit
-		exit -1
-	else
-		echo JAVA="$JAVA"
-fi
-$JAVA -version
 echo "***********"
 PWD=`pwd`
 echo PWD=$PWD
 echo "***********"
-CMD="$JAVA -Xms512M -Xmx512M -XX:PermSize=64M -XX:MaxPermSize=128M -XX:+UseConcMarkSweepGC -XX:+UseCMSCompactAtFullCollection -XX:CMSInitiatingOccupancyFraction=70 -XX:+CMSParallelRemarkEnabled -XX:SoftRefLRUPolicyMSPerMB=0 -XX:+CMSClassUnloadingEnabled -XX:SurvivorRatio=8 -XX:+DisableExplicitGC -XX:-OmitStackTraceInFastThrow -Djava.net.preferIPv4Stack=true -jar *.jar"
+CMD="JAVA -Xms512M -Xmx512M -XX:PermSize=64M -XX:MaxPermSize=128M -XX:+UseConcMarkSweepGC -XX:+UseCMSCompactAtFullCollection -XX:CMSInitiatingOccupancyFraction=70 -XX:+CMSParallelRemarkEnabled -XX:SoftRefLRUPolicyMSPerMB=0 -XX:+CMSClassUnloadingEnabled -XX:SurvivorRatio=8 -XX:+DisableExplicitGC -XX:-OmitStackTraceInFastThrow -Djava.net.preferIPv4Stack=true -jar *.jar"
 echo CMD=$CMD
-nohup $CMD >/dev/null 2>&1 &
+$CMD
 echo $!>$PID_FILE
 echo "***********"
 echo process id [`cat $PID_FILE`]
 #stty echo
-
